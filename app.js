3917fb5cd40c2c5e064f8f3ace0416779767e02c
Pete Du Beau <peterdubeau@yahoo.com>
2020-06-09 11:52:51 -0400

    Basic HTML set, API acessed, drop downs added

--- app.js
+++ app.js
@@ -1,1 +1,28 @@
-console.log("got it")
+const getOption = async () => {
+  
+
+  try {
+    
+    const url = "http://api.brewerydb.com/v2/styles/?key=f5be82be5b9ee3151bbe291b9f9596fa"
+    const res = await axios.get(url)
+    styleList = (res.data.data)
+    
+    console.log(res)
+    // console.log(styleList)
+    const selectStyle = document.querySelector('#select-style')
+
+    styleList.forEach((style) => {
+      const option = document.createElement('option')
+      for (let i = 0; i < styleList.length; i++) {
+        option.value = `${style}`
+        option.text = `${style}`
+        selectStyle.append(option)
+      }
+    })
+     
+  } catch (error) {
+    console.log(`THERE WAS AN ERROR: ${error}`)
+  }
+
+}
+getOption()
