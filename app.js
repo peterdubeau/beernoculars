7d60ba89e2e3316d3e78e02c7b183d464ba8979c
Pete Du Beau <peterdubeau@yahoo.com>
2020-06-09 15:50:57 -0400

    basic CSS set up

--- app.js
+++ app.js
@@ -1,25 +1,37 @@
 const getOption = async () => {
-  
 
   try {
-    
+    // accessing the BreweryDB API with an endpoint 
+    // of "beers"
     const url = "http://api.brewerydb.com/v2/styles/?key=f5be82be5b9ee3151bbe291b9f9596fa"
     const res = await axios.get(url)
     styleList = (res.data.data)
     
-    console.log(res)
-    // console.log(styleList)
-    const selectStyle = document.querySelector('#select-style')
+ 
+    const selectStyle = document.querySelector('#select-style')    // Logging the beers to make sure we have access
+    const selectABV = document.querySelector("#select-abv")         // console.log(Object.values(styleList))
 
-    styleList.forEach((style) => {
+    // Turning the list of beers into an array of objects
+    // and adding to the drop down
+    // styleList.forEach((style) => {
       const option = document.createElement('option')
-      for (let i = 0; i < styleList.length; i++) {
-        option.value = `${style}`
-        option.text = `${style}`
-        selectStyle.append(option)
-      }
-    })
-     
+    for (let i = 0; i < styleList.length; i++) {
+      option.value = `${styleList.name}`
+      option.text = `${styleList.name}`
+      selectStyle.append(option)
+    }
+        
+      
+    // })
+    // 
+    // styleList.forEach((abv) => {
+    //   const option = document.createElement('option')
+    //     option.value = `${abv}`
+    //     option.text = `${abv}`
+    //     selectABV.append(option)
+    // })
+
+
   } catch (error) {
     console.log(`THERE WAS AN ERROR: ${error}`)
   }
