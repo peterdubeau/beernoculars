9b6b39522fff36319e7018189f86e201242d4211
Pete Du Beau <peterdubeau@yahoo.com>
2020-06-09 20:41:05 -0400

    ABV hard coded into dropdown menu. Code cleaned up a bit

--- app.js
+++ app.js
@@ -9,39 +9,31 @@ const getOption = async () => {
     // console.log(styleList)
  
     const selectStyle = document.querySelector('#select-style')    // Logging the beers to make sure we have access
-    const selectABV = document.querySelector("#select-abv")         // console.log(Object.values(styleList))
-
-    // Turning the list of beers into an array of objects
-    // and adding to the drop down
-    // styleList.forEach((style) => {
     
     let beerInfo = []
-    
+
+    // This block of code populates the Style Drop Down with "glass" value from API
+    // I used glass value because it was much closer to an accurate style description.
     for (let i = 0; i < styleList.length; i++) {
       beerInfo = styleList[i].style
-      const option = document.createElement('option')
+      const option = document.createElement('option') 
       option.value = `${beerInfo.shortName}`
       option.text = `${beerInfo.shortName}`
       selectStyle.append(option)
       console.log(beerInfo.name)
     }
-    
-   
-   
-      
-    // })
-    // 
-    // styleList.forEach((abv) => {
-    //   const option = document.createElement('option')
-    //     option.value = `${abv}`
-    //     option.text = `${abv}`
-    //     selectABV.append(option)
-    // })
-
-
+  
   } catch (error) {
     console.log(`THERE WAS AN ERROR: ${error}`)
   }
 
 }
 getOption()
+
+// Hard coding the ABV options into the 2nd drop down menu
+// CLEAN THIS UP IN POST MVP
+const selectABV = document.querySelector("#select-abv")  
+  selectABV.options[selectABV.options.length] = new Option("Less than 5%", 1)
+  selectABV.options[selectABV.options.length] = new Option("5% to 7%", 2)
+  selectABV.options[selectABV.options.length] = new Option("7.1% to 9.9%", 3)
+  selectABV.options[selectABV.options.length] = new Option("More than 10%", 4)
